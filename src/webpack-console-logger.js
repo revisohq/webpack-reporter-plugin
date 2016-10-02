9a14689e7da2cf1091d1c863953eb39434ecc2a5
Benjamin Horsleben <benjamin@fizkerinc.dk>
2016-10-03 00:46:19 +0200

    Improved logging

--- src/webpack-console-logger.js
+++ src/webpack-console-logger.js
@@ -1,13 +1,33 @@
+import { stdout as singleLineLog } from 'single-line-log'
+
 import WebpackNotifier from './webpack-notifier'
 
+function getTime() {
+	var now = new Date();
+	return [now.getHours(), now.getMinutes(), now.getSeconds()]
+		.map(function(num) {
+			return ('0' + num).slice(-2);
+		})
+		.join(':');
+}
+
 export default class WebpackConsoleLogger extends WebpackNotifier {
-	constructor() {
+	constructor({ useSingleLine = true, keepOldBuildMessages = false } = {}) {
+		const log = useSingleLine ? message => singleLineLog(message + '\n') : console.log.bind(console)
+
 		super({
 			onStart: () => {
-				console.log('Starting build')
+				log(getTime() + ': Starting build')
 			},
 			onFinish: ({ warnings = [], errors = [] } = {}) => {
-				console.log(`Build finished. ${warnings.length} warning(s), ${errors.length} error(s)`)
+				let lines = [
+					formattedErrors,
+					`Webpack finished. ${warnings.length} warning(s), ${errors.length} error(s)`,
+				]
+				log(lines.map(x => `${getTime()}: ${x}`).join('\n'))
+				if(useSingleLine && keepOldBuildMessages) {
+					console.log('')
+				}
 			},
 		})
 	}
