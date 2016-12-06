c99fe50747f033bac965ecae72aeaf1b29e21c4a
Benjamin Horsleben <benjamin@fizkerinc.dk>
2016-12-06 10:03:11 +0100

    Fixed an issue where messages without a module would crash the reporter

--- src/format.js
+++ src/format.js
@@ -4,6 +4,10 @@ export default function(errors) {
 	return errors
 		.map(x => {
 			let message = x.message
+			if(!x.module) {
+				return message
+			}
+
 			let file = x.module.userRequest
 			let relativeFilename = relativizeFilename(file)
 			return `${message}\n  in ${relativeFilename}`
